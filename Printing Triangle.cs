004f1aa504b4f54d1240c3459664488dfd9756e7
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-13 20:13:54 +0300

    Add files via upload

---
+++ Printing Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int end = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= end; i++)
+            {
+                PrintLine(i);
+            }
+
+            for (int i = end -1; i >= 1; i--)
+            {
+                PrintLine(i);
+            }
+        }
+
+        private static void PrintLine(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
