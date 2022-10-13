1d4d1737588141d950f13f21069f411cc172f679
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-13 22:42:18 +0300

    Add files via upload

---
+++ Math Power.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double @base = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            double result = MathPower(@base, power);
+            Console.WriteLine(result);
+        }
+        static double MathPower(double @base, double power)
+        {
+            return Math.Pow(@base, power);
+        }
+    }
+}
