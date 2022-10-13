11e9266f722de7d6c34e216b5206b271d8719c15
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-13 20:18:29 +0300

    Add files via upload

---
+++ Repeat String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+
+            int n = int.Parse(Console.ReadLine());
+            string result = PrintArray(array, n);
+        }
+        static string PrintArray(string array, int n)
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(array);
+            }
+            return result;
+        }
+    }
+}
