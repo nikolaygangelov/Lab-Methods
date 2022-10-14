7dcdc4a0cb535b85f57471dfb677ed30cf96d095
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-14 23:13:15 +0300

    Add files via upload

---
+++ Math Operations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11._Math_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            char @operator = char.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            double result = Calculate(a, @operator, b);
+            Console.WriteLine(result);
+        }
+
+        private static double Calculate(int a, char @operator, int b)
+        {
+            double result = 0;
+            if (@operator == '/')
+            {
+                result = a / b;
+            }
+            else if (@operator == '*')
+            {
+                result = a * b;
+            }
+            else if (@operator == '+')
+            {
+                result = a + b;
+            }
+            else if (@operator == '-')
+            {
+                result = a - b;
+            }
+
+            return result;
+        }
+    }
+}
